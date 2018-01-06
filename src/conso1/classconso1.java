55c38d982430c4eea093a4e1f10583214cdab9e9
Fauzi24 <fauziaris44@gmail.com>
2018-01-06 14:13:48 +0700

    V 1

---
+++ src/conso1/classconso1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package conso1;
+
+/**
+ *
+ * @author Fauzi
+ */
+public class classconso1 {
+    public static void main(String[] args) {
+        int bil1,bil2,hasil;
+        bil1 = 5;
+        bil2 = 2;
+        hasil = bil1 * bil2;
+        System.out.println("Hasil perkalian "+ bil1 +" dan "+ bil2 +" adalah "+ hasil);
+                
+    }
+}
